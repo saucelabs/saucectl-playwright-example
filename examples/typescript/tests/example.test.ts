67132d5eab068b6c7499334cae67b6356831edb5
Valentyn Kremeshnyi <valentyn.kremeshnyi@saucelabs.com>
2021-05-14 14:16:01 -0700

    add typescript example

---
+++ examples/typescript/tests/example.test.ts
@@ -0,0 +1,8 @@
+const { it, expect } = require('@playwright/test');
+
+it('should verify title of the page', async ({ page }) => {
+    const url:string = "https://www.saucedemo.com/";
+    const title:string = "Swag Labs";
+    await page.goto(url);
+    expect(await page.title()).toBe(title);
+});
