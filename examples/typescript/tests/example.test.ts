96e4e581dd354f210ec963b78384c9ddce9eb1f2
Valentyn Kremeshnyi <valentyn.kremeshnyi@saucelabs.com>
2021-05-17 16:13:52 -0700

    add playwright to the worflow

--- examples/typescript/tests/example.test.ts
+++ examples/typescript/tests/example.test.ts
@@ -1,5 +1,6 @@
 const { it, expect } = require('@playwright/test');
 
+// @ts-ignore
 it('should verify title of the page', async ({ page }) => {
     const url:string = "https://www.saucedemo.com/";
     const title:string = "Swag Labs";
