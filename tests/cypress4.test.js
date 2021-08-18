5cb696d9098f8123c51e7b2b0b250e559a6c4a4d
Alex Plischke <alex.plischke@saucelabs.com>
2021-08-18 15:15:57 -0700

    Dupe tests

--- tests/cypress4.test.js
+++ tests/cypress4.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 const exp = require('constants');
 
-test('.type() - type into a DOM element 2', async ({ page }) => {
+test('.type() - type into a DOM element 4', async ({ page }) => {
     await page.goto('https://example.cypress.io/commands/actions');
     await page.fill('.action-email', "fake@email.com");
 });
